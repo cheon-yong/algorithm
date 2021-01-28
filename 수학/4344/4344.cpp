15c11dc06a64f97b3a8b059eee29d994a879a40d
yongjun <yes1312@naver.com>
2021-01-28 21:53:00 +0900

    '4344'

--- 수학/4344/4344.cpp
+++ 수학/4344/4344.cpp
@@ -1,8 +1,37 @@
+/*
+    백준 4344번 문제 평균
+    작성자 : cheon-yong <yes1312@naver.com>
+    입력받은 수들로부터 평균값을 구하고 
+    평균값을 초과한 수들의 비율을 출력한다.
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+    for (int i=0; i<t; i++)
+    {
+        int n;
+        cin >> n;
+        float arr[n];
+        float sum = 0;;
+        for (int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+            sum += arr[i];
+        }
+        float avg = sum / n;
+        float cnt = 0;
+        for (int i=0; i<n; i++)
+        {
+            if (arr[i] > avg)
+                cnt++;    
+        }
+        printf("%.3f%%\n", cnt/n*100);
+    }
     return 0;
 }
