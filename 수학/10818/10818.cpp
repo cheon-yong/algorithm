3dd231bd5bff8251941c1ac6e3e3902804af2f7d
전용준 <yes1312@naver.com>
2022-01-02 21:04:41 +0900

    10818

---
+++ 수학/10818/10818.cpp
@@ -0,0 +1,26 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int *numbers;
+    cin >> n;
+
+    numbers = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> numbers[i];
+
+    int min = 1000000, max = -1000000;
+    for (int i = 0; i < n; i++)
+    {
+        if (numbers[i] >= max)
+            max = numbers[i];
+        if (numbers[i] <= min)
+            min = numbers[i];
+    }
+
+    cout << min << " " << max << endl;
+    return 0;
+}
