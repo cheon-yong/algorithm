e1731f116b14881e3160db49091dc719ada169ea
전용준 <yes1312@naver.com>
2022-01-07 14:25:44 +0900

    2869

--- 수학/2869/2869.cpp
+++ 수학/2869/2869.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main()
 {
+    int a, b, v;
+    cin >> a >> b >> v;
+
+    int day = v / (a - b);
+    if (v % (a - b) > 0)
+        day++;
+
+    cout << day << endl;
+
     return 0;
 }
