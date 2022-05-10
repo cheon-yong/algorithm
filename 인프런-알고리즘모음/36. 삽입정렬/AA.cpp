74f7f7ef721f799a968758bb8cce349384ceb59d
cheonyongjun <yes1312@naver.com>
2022-05-11 00:28:02 +0900

    36. 삽입정렬 수정

--- 인프런-알고리즘모음/36. 삽입정렬/AA.cpp
+++ 인프런-알고리즘모음/36. 삽입정렬/AA.cpp
@@ -12,21 +12,20 @@ int main()
         cin >> arr[i];
     }
 
+    int j = 0;
     for (int i = 1 ; i < n; i++)
     {
         int temp = arr[i];
-        int lastIndex = 0;
-        for (int j = i - 1; j >= 0; j--)
+        for (j = i - 1; j >= 0; j--)
         {
             if (arr[j] > temp)
                 arr[j+1] = arr[j];
             else
             {
-                lastIndex = j + 1;
                 break;  
             }
         }
-        arr[lastIndex] = temp;
+        arr[j+1] = temp;
     }
     for (int i = 0; i < n; i++)
     {
