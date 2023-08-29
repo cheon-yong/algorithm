0b35d54612e843a77d32bd1369c10d583a162060
cheonyongjun <yes1312@naver.com>
2023-08-29 14:26:27 +0900

    JadenCase 문자열 만들기

---
+++ programmers/Lv2/JadenCase문자열만들기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include<bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    if (s[0] >= 'a' && s[0] <= 'z')
+        s[0] -= 32;
+    
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i - 1] == ' ' && s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] -= 32;
+        }
+    }
+    
+    answer = s;
+    
+    return answer;
+}
